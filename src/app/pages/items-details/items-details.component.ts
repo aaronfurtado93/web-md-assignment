3550a4444c3bce9605327d7d3f29c13bce945daa
Aaron Furtado <aaronfurtado93@hotmail.com>
2021-07-11 21:06:36 +0530

    display data for selected item

--- src/app/pages/items-details/items-details.component.ts
+++ src/app/pages/items-details/items-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ItemsListService} from "../../services/items-list/items-list.service";
 import {IItem} from "../../data-models/iitem";
+import {ActivatedRoute} from "@angular/router";
+import {map, switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-items-details',
@@ -11,13 +13,32 @@ export class ItemsDetailsComponent implements OnInit {
 
   items: IItem[] = [];
 
+  selectedItem: IItem | undefined;
+
   constructor(
+    private activatedRoute: ActivatedRoute,
     private itemsListService: ItemsListService
   ) { }
 
   ngOnInit(): void {
-    this.itemsListService.get()
-      .subscribe(value => this.items = value)
+    this.activatedRoute.paramMap
+      .pipe(
+        switchMap((paramMap) => {
+          return this.itemsListService.get()
+            .pipe(
+              map((items) => {
+                return {
+                  items,
+                  selectedItem: items.find(item => item.title === paramMap.get(`title`))
+                }
+            }))
+        })
+      )
+      .subscribe(({items, selectedItem}) => {
+        this.items = items;
+        this.selectedItem = selectedItem;
+        return;
+      })
   }
 
 }
